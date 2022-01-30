044dad6c0b3f454d7f8111a7a2c99ef36edf04e6
henan-wq <nanhe31@gmail.com>
2022-01-30 16:17:17 +0800

    new file:   6-2/ex6-2-3/README.md         new file:   6-2/ex6-2-4/README.md         new file:   6-2/ex6-2-5/README.md         new file:   6-2/ex6-2-5/ex6-2-5.rkt

---
+++ 6-2/ex6-2-5/README.md
@@ -0,0 +1,20 @@
+# 练习 6.2.5
+下面是函数`next`：
+```
+;; next : symbol -> symbol
+;; 将当前红绿灯颜色转换为下一颜色
+(define (next current-color)
+  (cond
+    [(and (symbol=? current-color 'red) (switch 'red 'green))
+     'green]
+    [(and (symbol=? current-color 'yellow) (switch 'yellow 'red))
+     'red]
+    [(and (symbol=? current-color 'green) (switch 'green 'yellow))
+     'yellow])) 
+```
+函数的输入为红绿灯的当前颜色，输出为红绿灯的下一个颜色。  
+	- 即如果输入为`'green`，输出为`'yellow`；
+	- 输入为`'yellow`，输出为`'red`；
+	- 输入为`'red`，输出为`'green`。  
+  
+将`图 6.1` 的最后3行代码替换成`(draw-bulb red)`，是红绿灯的当前状态为红灯亮，然后使用`next`函数将红绿灯颜色进行 4 次转换。 
---
+++ 6-2/ex6-2-5/ex6-2-5.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex6-2-5) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "draw.rkt" "teachpack" "htdp")) #f)))
+;; next : symbol -> symbol
+;; 将当前红绿灯颜色转换为下一颜色
+(define (next current-color)
+  (cond
+    [(and (symbol=? current-color 'red) (switch 'red 'green))
+     'green]
+    [(and (symbol=? current-color 'yellow) (switch 'yellow 'red))
+     'red]
+    [(and (symbol=? current-color 'green) (switch 'green 'yellow))
+     'yellow]))
