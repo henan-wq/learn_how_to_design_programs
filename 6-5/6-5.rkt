611f97b0045f717c892fbdedce4486008a6e4f08
henan-wq <nanhe31@gmail.com>
2022-02-03 18:34:44 +0800

    new file:   6-5/6-5.rkt         modified:   6-5/README.md

--- 6-5/6-5.rkt
+++ 6-5/6-5.rkt
@@ -1,32 +1,32 @@
-# 6.5 设计处理符合数据的函数
-> 简单原则：如果某个对象的描述需要若干种信息则使用结构体。  
-1. 如果不使用结构体，程序设计者可能很快会对哪些信息属于哪种对象失去线索，当编写大量数据的大型函数时，尤其如此。  
-2. 程序设计者可以使用结构体定义和数据定义来组织函数。在设计函数的时候可以使用模板，正如本节和以后章节中所看到那样，模板和函数定义相匹配，这是函数设计中的基本要素。  
-  
-完整例子如下：  
-```
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 6-5) (read-case-sensitive #t) (teachpacks ((lib "draw.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "draw.rkt" "teachpack" "htdp")) #f)))
 ;; 数据分析和定义：
 (define-struct student (last first teacher))
-;; student 是结构体(make-student last first teacher)，其中last first teacher是符号
+;; student是结构体他(make-student last first teacher)，
+;; 其中last first teacher是符号
 
 ;; 合约： subst-teacher : student symbol -> student
 
 ;; 用途说明：如教师的名字为'Fritz，创建一个student结构体，把教师的名字替换为新的
 
 ;; 例子：
+;; 第一种情况修改了
 ;; (subst-teacher (make-student 'Find 'Matthew 'Fritz) 'Elise)
-;; = 
+;; =
 ;; (make-student 'Find 'Matthew 'Elise)
-;; (subst-teacher (make-student 'Fien 'Matthew 'Amanda) 'Elise)
+;; 第二种情况没有修改
+;; (subst-teacher (make-student 'Find 'Matthew 'Amanda) 'Elise)
 ;; =
 ;; (make-student 'Find 'Matthew 'Amanda)
+
 ;; 模板：
 ;; (define (process-student a-student a-teacher)
 ;; ...(student-last a-student)...
 ;; ...(student-first a-student)...
 ;; ...(student-teacher a-student)...)
-;; 定义：
 
+;; 定义
 (define (subst-teacher a-student a-teacher)
   (cond
     [(symbol=? (student-teacher a-student) 'Fritz)
@@ -35,13 +35,12 @@
                    a-teacher)]
     [else a-student]))
 
-
-;; 测试：
+;; 测试
+;; 测试会被修改的
 (subst-teacher (make-student 'Find 'Matthew 'Fritz) 'Elise)
-;; 预期值：
+;; 预期值
 (make-student 'Find 'Matthew 'Elise)
-
+;; 测试不会被修改的
 (subst-teacher (make-student 'Find 'Matthew 'Amanda) 'Elise)
-;; 预期值：
-(make-student 'Find 'Matthew 'Amanada)
-```
+;; 预期值
+(make-student 'Find 'Matthew 'Amanda)
